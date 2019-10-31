0f60482357e5a7a3cbd1645bc65d86e2d1c04dea
admin <wafalouarrat@gmail.com>
2019-11-01 00:10:49 +0100

    Creation des classes

---
+++ src/start/Main.java
@@ -0,0 +1,18 @@
+package start;
+
+/**
+ * lancement du moteur avec le jeu
+ */
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		// creation du jeu particulier et de son afficheur
+
+
+		// classe qui lance le moteur de jeu generique
+
+
+	}
+
+}
