4ba34e8124ccf447896ce471cf3754a5e2d50c90
Bert54 <matt.bert@live.fr>
2019-11-14 15:18:35 +0100

    Added new Tiles

--- src/game/Hero.java
+++ src/game/Hero.java
@@ -29,6 +29,5 @@ public class Hero extends GameEntity {
             crayon.setColor(Color.red);
         }
         crayon.fillOval(position.x, position.y, box.x, box.y);
-        System.out.println(this.speedFrames);
     }
 }
